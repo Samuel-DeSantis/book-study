f989796fdefc4c8a2adb66d9ed32d6053aec2f30
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-04-19 11:53:34 -0600

    Bible API books and chapters, verses returning CORS error

--- frontend/src/components/bible_api/BibleAPI.js
+++ frontend/src/components/bible_api/BibleAPI.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
-
+// import ScriptureAPI from 'scripture-api'
+// const ScriptureAPI = require('scripture-api');
+// const scripture_api = new ScriptureAPI('188d484b898780abe077fefa6641187d')
 export default class BibleAPI extends Component {
+
+  // componentDidMount () {
+  //   const params = {
+  //     language: 'eng',
+  //     abbreviation: 'kjv',
+  //     name: 'king james version'
+  //   }
+
+  //   scripture_api.getBibles()
+  //   .then(data => console.log(data))
+  //   .catch(error => console.log(error))
+  // }
   
   componentDidMount () {
-    URL = `https://api.scripture.api.bible/v1/bibles`
-    fetch(URL, {header: {'api-key': '188d484b898780abe077fefa6641187d'}})
+
+    const bibles ={
+      'kjv': 'de4e12af7f28f599-02'
+    }
+
+
+    URL = `https://api.scripture.api.bible/v1/bibles/${bibles['kjv']}/books/GEN/chapters`
+    fetch(URL, {
+      method: 'GET',
+      headers: {
+        'api-key': '188d484b898780abe077fefa6641187d',
+      }
+    })
     .then(response => response.json())
     .then(data => console.log(data))
   }
